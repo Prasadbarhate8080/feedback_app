8dfc3709b2c15fb4b770342711f49da7d9e0668e
prasad barhate <prasadbarhate388@gmail.com>
2026-01-25 23:45:54 +0530

    all good4

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -1,20 +1,23 @@
 import { userModel } from "@/models/user.model";
 import { dbConnect } from "@/lib/dbConnect";
-import { getServerSession, User } from "next-auth";
+import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
-
+import { NextRequest } from "next/server";
 
 export async function DELETE(
-  request: Request,
-  { params }: { params: { messageid: string } }
+  request: NextRequest,
+  context: { params: { messageid: string } }
 ) {
-  const messageId = params.messageid;
+  const { messageid } = context.params;
+
   await dbConnect();
+
   const session = await getServerSession(authOptions);
-  const _user: User = session?.user;
+  const _user = session?.user;
+
   if (!session || !_user) {
     return Response.json(
-      { success: false, message: 'Not authenticated' },
+      { success: false, message: "Not authenticated" },
       { status: 401 }
     );
   }
@@ -22,24 +25,24 @@ export async function DELETE(
   try {
     const updateResult = await userModel.updateOne(
       { _id: _user._id },
-      { $pull: { messages: { _id: messageId } } }
+      { $pull: { messages: { _id: messageid } } }
     );
 
     if (updateResult.modifiedCount === 0) {
       return Response.json(
-        { message: 'Message not found or already deleted', success: false },
+        { message: "Message not found or already deleted", success: false },
         { status: 404 }
       );
     }
 
     return Response.json(
-      { message: 'Message deleted', success: true },
+      { message: "Message deleted", success: true },
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error deleting message:', error);
+    console.error("Error deleting message:", error);
     return Response.json(
-      { message: 'Error deleting message', success: false },
+      { message: "Error deleting message", success: false },
       { status: 500 }
     );
   }

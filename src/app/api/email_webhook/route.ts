81b28ed38c2d463dc918434c8d6587328d15616a
prasad barhate <prasadbarhate388@gmail.com>
2026-01-28 23:44:05 +0530

    fix: correct console log message in email webhook POST function

--- src/app/api/email_webhook/route.ts
+++ src/app/api/email_webhook/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function POST(){
+
+    console.log("email webhook cllled and email send successfully and ");
     return NextResponse.json("data: ths is the json response");
 }
